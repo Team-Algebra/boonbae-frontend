0cb344dbda9121e688ac8c4ea8fb20afd0ebc422
OhSeChan <seb_s19@naver.com>
2023-07-04 05:44:52 +0900

    feat: LoginFrom 기본 기능 구현
    - token을 local storage에 저장하는 방식으로 구현
    - 로그인 성공 시 메인 페이지로 이동

--- boonbae/src/pages/Login/components/LoginForm.jsx
+++ boonbae/src/pages/Login/components/LoginForm.jsx
@@ -1,10 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios'
 
 const LoginForm = () => {
+
+  const [formData, setFormData] = useState({
+    id : '',
+    password : '',
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleLogin = async(e) =>{
+    e.preventDefault();
+    try {
+      const response = await axios.post(`${process.env.REACT_APP_PROXY}/users/login`,{
+        id : formData.id,
+        password : formData.password
+      });
+      const token = response.data.token;
+      localStorage.setItem('token', token);
+      if (response.status === 200) {
+        navigate('/');
+      }else {
+        alert('로그인에 실패했습니다.');
+      }
+    } catch (error) {
+      alert('로그인에 실패했습니다.');
+      console.log(error);
+    }
+  }
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <form className='login-form' onSubmit={handleLogin}>
+        <input
+          type="text"
+          name="id"
+          placeholder="아이디"
+          value={formData.id}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          autoComplete="new-password"
+          placeholder="비밀번호"
+          value={formData.password}
+          onChange={handleChange}
+        />
+        <button
+          type="submit"
+          className='login-loginbtn'
+          disabled={!(formData.id && formData.password)}
+        >
+          로그인
+        </button>
+      </form>
+      <button
+        type="button"
+        className='login-signupbtn'
+        onClick={()=>{navigate('/signup')}}
+      >
+        회원가입
+      </button>
+    </>
   );
 };
 
