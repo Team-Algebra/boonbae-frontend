ea87acc4bcba72a0fc7cea4ab63de8e8fcb60f03
OhSeChan <seb_s19@naver.com>
2023-07-03 19:22:32 +0900

    feat: 중복 확인 및 회원 가입 기능 추가
    - SignupInputField 컴포넌트 생성
    - 아이디, 닉네임 중복 확인 기능 추가
    - 추천인 아이디 확인 기능 추가
    - 회원 가입 기능 추가
    - input 값이 바뀌었을 경우 해당 조건 초기화 기능 추가
    - 회원가입 비활성화 기능 추가

--- boonbae/src/pages/Signup/components/SignupForm.jsx
+++ boonbae/src/pages/Signup/components/SignupForm.jsx
@@ -1,16 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import InputField from './SignupInputField'
 
 const SignupForm = () => {
-  const [formData, setFormData] = useState({
+  const initialFormData = {
     id: '',
     username: '',
     password: '',
     confirmPassword: '',
     referralID: '',
-    isIdAvail: true,
-    isUsernameAvail: true,
+    isIdDuplicate: true,
+    isUsernameDuplicate: true,
     isReferralValid: false,
-  });
+  };
+
+  const [formData, setFormData] = useState(initialFormData);
+  const [isSignupDisabled, setIsSignupDisabled] = useState(true);
+
+  const navigate = useNavigate();
 
   const regex = {
     id_rule: '*영문자 시작하는 영문자 또는 숫자 6~20자입니다.',
@@ -22,11 +30,26 @@ const SignupForm = () => {
     password_reg: /^(?=.*[a-zA-Z])(?=.*\d)(?=.*\W).{8,}$/,
   };
 
+  const apiEndpoints = {
+    isIdDuplicate: `${process.env.REACT_APP_PROXY}/users/id/${formData.id}/exists`,
+    isUsernameDuplicate: `${process.env.REACT_APP_PROXY}/users/username/${formData.username}/exists`,
+    isReferralValid: `${process.env.REACT_APP_PROXY}/users/referrers?referrer=${formData.referralID}`,
+    signup: `${process.env.REACT_APP_PROXY}/users`
+  };
+
+  /**
+  * 입력 필드의 변경 이벤트 핸들러입니다.
+  * 폼 데이터를 업데이트하고 상태를 업데이트합니다.
+  * @param {Object} e - 변경 이벤트 객체
+  */
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({
       ...prevData,
       [name]: value,
+      isIdDuplicate: name === 'id' ? true : prevData.isIdDuplicate,
+      isUsernameDuplicate: name === 'username' ? true : prevData.isUsernameDuplicate,
+      isReferralValid: name === 'referralID' ? false : prevData.isReferralValid,
     }));
   };
 
@@ -43,85 +66,156 @@ const SignupForm = () => {
     return null;
   };
 
+  /**
+  * 지정된 필드에 대한 중복 여부를 확인하고 해당 필드 값을 업데이트합니다.
+  * @param {string} field - 확인할 필드 이름
+  */
+  const checkAvailability = async (field) => {
+    const endpoint = apiEndpoints[field];
+    if (!endpoint) return;
+
+    try {
+      const response = await axios.get(endpoint);
+      const isAvailable = response.data.exists;
+
+      if (field === 'isReferralValid') {
+        if (isAvailable) {
+          alert('사용이 가능합니다.');
+        } else {
+          setFormData((prevData) => ({
+            ...prevData,
+            [field]: isAvailable,
+            referralID: '',
+          }));
+          alert('사용이 불가합니다.');
+        }
+      } else {
+        setFormData((prevData) => ({
+          ...prevData,
+          [field]: isAvailable,
+        }));
+
+        if (isAvailable) {
+          alert('사용이 불가합니다.');
+        } else {
+          alert('사용이 가능합니다.');
+        }
+      }
+    } catch (error) {
+      console.error(`${field} 확인에 실패했습니다.`, error);
+    }
+  };
+
+  useEffect(() => {
+    const isDisabled =
+      !formData.isIdDuplicate &&
+      !formData.isUsernameDuplicate &&
+      formData.password !== '' &&
+      regex.password_reg.test(formData.password) &&
+      formData.password === formData.confirmPassword;
+
+    setIsSignupDisabled(!isDisabled);
+  }, [formData]);
+
+
+  /**
+  * 회원가입을 처리합니다.
+  * @param {Object} e - 이벤트 객체
+  */
+  const handleSignup = async (e) => {
+    e.preventDefault();
+
+    const endpoint = apiEndpoints.signup;
+    if (!endpoint) return;
+
+    try {
+      const response = await axios.post(endpoint, {
+        id: formData.id,
+        username: formData.username,
+        password: formData.password,
+        referrer_id: formData.referralID,
+      });
+
+      if (response.status === 201) {
+        alert('회원가입에 성공했습니다!');
+        navigate('/login');
+      } else if (response.status === 409) {
+        alert('이미 존재하는 계정입니다. 다른 아이디를 선택해주세요.');
+      } else {
+        alert('회원가입에 실패했습니다.');
+      }
+    } catch (error) {
+      console.error('회원가입에 실패했습니다.', error);
+    }
+  };
 
   return (
     <form className="signup-form">
-      <div>
-        <input
-          type="text"
-          name="id"
-          placeholder="아이디"
-          value={formData.id}
-          onChange={handleChange}
-        />
-        <button
-          className="signup-inputbtn"
-          disabled={!regex.id_reg.test(formData.id)}
-        >
-          중복 확인
-        </button>
-      </div>
+      <InputField
+        type="text"
+        name="id"
+        placeholder="아이디"
+        value={formData.id}
+        onChange={handleChange}
+        buttonLabel="중복 확인"
+        isButtonDisabled={!regex.id_reg.test(formData.id)}
+        onButtonClick={() => checkAvailability('isIdDuplicate')}
+      />
       {guideMsg('id', regex.id_rule)}
-      <div>
-        <input
-          type="text"
-          name="username"
-          placeholder="닉네임"
-          value={formData.username}
-          onChange={handleChange}
-        />
-        <button
-          className="signup-inputbtn"
-          disabled={!regex.username_reg.test(formData.username)}
-        >
-          중복 확인
-        </button>
-      </div>
+
+      <InputField
+        type="text"
+        name="username"
+        placeholder="닉네임"
+        value={formData.username}
+        onChange={handleChange}
+        buttonLabel="중복 확인"
+        isButtonDisabled={!regex.username_reg.test(formData.username)}
+        onButtonClick={() => checkAvailability('isUsernameDuplicate')}
+      />
       {guideMsg('username', regex.username_rule)}
-      <div>
-        <input
-          type="password"
-          name="password"
-          autoComplete="off"
-          placeholder="비밀번호"
-          value={formData.password}
-          onChange={handleChange}
-        />
-      </div>
+
+      <InputField
+        type="password"
+        name="password"
+        autoComplete="off"
+        placeholder="비밀번호"
+        value={formData.password}
+        onChange={handleChange}
+      />
       {guideMsg('password', regex.password_rule)}
+
       <label htmlFor="signup-confirmpassword">비밀번호 재확인</label>
-      <div>
-        <input
-          type="password"
-          name="confirmPassword"
-          autoComplete="off"
-          placeholder="비밀번호 재확인"
-          value={formData.confirmPassword}
-          onChange={handleChange}
-        />
-      </div>
-      {
-        formData.confirmPassword !== '' 
-        && formData.password !== formData.confirmPassword 
-        && (<p>{regex.confirmPassword_rule}</p>)
-      }
-      <label htmlFor="signup-referralID">추천인 아이디</label>
-      <div>
-        <input
-          type="text"
-          name="referralID"
-          placeholder="추천인 아이디"
-          value={formData.referralID}
-          onChange={handleChange}
-        />
-      <button
-        className="signup-inputbtn"
-        disabled={!regex.id_reg.test(formData.referralID)}
+      <InputField
+        type="password"
+        name="confirmPassword"
+        autoComplete="off"
+        placeholder="비밀번호 재확인"
+        value={formData.confirmPassword}
+        onChange={handleChange}
+      />
+      {formData.confirmPassword !== '' && formData.password !== formData.confirmPassword && (
+        <p>{regex.confirmPassword_rule}</p>
+      )}
+
+      <label htmlFor="signup-referralID">추천인 아이디 (선택)</label>
+      <InputField
+        type="text"
+        name="referralID"
+        placeholder="추천인 아이디"
+        value={formData.referralID}
+        onChange={handleChange}
+        buttonLabel="확인"
+        isButtonDisabled={!regex.id_reg.test(formData.referralID)}
+        onButtonClick={() => checkAvailability('isReferralValid')}
+      />
+
+      <button 
+        className="signup-submit"
+        type='submit'
+        disabled={isSignupDisabled}
+        onClick={handleSignup}
       >
-        확인
-      </button>
-      </div>
-      <button className="signup-submit" type="submit">
         회원가입
       </button>
     </form>
