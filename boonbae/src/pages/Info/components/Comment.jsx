132cbfd81868c7760eb72855eccb55f7b2cbbb83
bini59 <myuni0234@gmail.com>
2023-07-04 20:56:35 +0900

    feat : 좋아요 기능 추가
    - 좋아요 API 연결
    - 좋아요 해제 등의 기능 설정
    - 테스트 필요, 실시간 반영 X 에대한 부분 수정 필요

--- boonbae/src/pages/Info/components/Comment.jsx
+++ boonbae/src/pages/Info/components/Comment.jsx
@@ -1,13 +1,42 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faThumbsUp as likeSolid } from "@fortawesome/free-solid-svg-icons"
 import { faThumbsUp as likeRegular } from "@fortawesome/free-regular-svg-icons"
+import { useState, useEffect } from "react"
+
+const API = process.env.REACT_APP_PROXY;
 
 export const Comment = ({ comment }) => {
-    const commentInfo = {
-        user: comment.username,
-        content: comment.content,
-        like: comment.like_cnt,
-        likeStatus: comment.like
+    const [commentInfo, setItemInfo] = useState({user: "", content: "", like: 0, likeStatus: false})
+
+    useEffect(() => {
+        if (comment.username === undefined) return;
+        setItemInfo({
+            user: comment.username,
+            content: comment.content,
+            like: comment.like_cnt,
+            likeStatus: comment.like,
+            id: comment.comment_pk
+        });
+    }, [comment]);
+
+    const clickLike = (e) => {
+        if (commentInfo.user === "") return;
+        
+        fetch(`${API}/recycling/comment/${commentInfo.id}/like`, {
+            method: commentInfo.likeStatus ? "DELETE" : "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ recycling_pk: commentInfo.id })
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.status >= 500) throw new Error({ error: "서버 오류", status: data.status });
+                if (data.status >= 400) throw new Error({ error: "클라이언트 오류", status: data.status });
+            })
+            .catch(err => {
+                console.log(err);
+                if (err.status >= 500) alert("서버 오류입니다.");
+                else alert("로그인이 필요한 서비스입니다.");
+            })
     }
 
     return (
@@ -18,7 +47,7 @@ export const Comment = ({ comment }) => {
             </div>
             <div className="info-comments-etc">
                 <div className="info-comments-like">
-                    <div className="info-comments-like-icon">
+                    <div className="info-comments-like-icon" onClick={clickLike}>
                         <FontAwesomeIcon 
                             icon={commentInfo.likeStatus? likeSolid : likeRegular} 
                             size="2x" 
