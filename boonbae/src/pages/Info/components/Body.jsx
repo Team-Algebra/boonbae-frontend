ec3115ca254dc760fdce0856dbe83d5f3d229ba6
bini59 <myuni0234@gmail.com>
2023-07-04 20:31:51 +0900

    feat : Body Component api data연결
    - api 데이터 연결
    - 데이터가 없을시 처리 완료

--- boonbae/src/pages/Info/components/Body.jsx
+++ boonbae/src/pages/Info/components/Body.jsx
@@ -1,20 +1,29 @@
-export const Body = () => {
+import { useEffect, useState } from "react"
+
+export const Body = ({ item }) => {
+    
+    const [itemInfo, setItemInfo] = useState({ process : [], detail : []});
+
+
+    useEffect(() => {
+        console.log(item)
+        if (item.name === undefined || !(item.process instanceof Object)) return;
+        setItemInfo({process: item.process, detail: item.description})
+
+    }, [item])
+
     return (
         <section className="info-body">
             <div className="info-body-way">
                 <div>버리는 법</div>
                 <ol>
-                    <li>버리는 법 1</li>
-                    <li>버리는 법 2</li>
-                    <li>버리는 법 3</li>
+                    {itemInfo.process.map((process, idx) => { return <li key={idx}>{process}</li>})}
                 </ol>
             </div>
             <div className="info-body-details">
                 <div>상세 설명</div>
                 <ol>
-                    <li>상세 설명 1</li>
-                    <li>상세 설명 2</li>
-                    <li>상세 설명 3</li>
+                    {itemInfo.detail.map((detail, idx) => { return <li key={idx}>{detail}</li>})}
                 </ol>
             </div>
         </section>
