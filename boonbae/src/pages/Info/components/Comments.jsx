be3b667b8c40015ed841ced51286239b57419263
bini59 <myuni0234@gmail.com>
2023-07-04 17:44:04 +0900

    feat : 댓글 API연결
    - 임시 데이터를 활용하여 API연결
    - 댓글 불러오기 기능 구현 완료

--- boonbae/src/pages/Info/components/Comments.jsx
+++ boonbae/src/pages/Info/components/Comments.jsx
@@ -2,13 +2,52 @@ import { Comment } from "./Comment"
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPaperPlane } from "@fortawesome/free-solid-svg-icons"
+import { useEffect, useState } from "react"
+
+
+const API_URL = process.env.REACT_APP_API_URL;
 
 export const Comments = () => {
+
+    const [comments, setComments] = useState();
+
+    const getComments = () => {
+        // get comments
+        // 테스트를 위해 임시로 2번 아이템의 댓글을 가져옴
+        fetch(`${API_URL}/recycling/2/comments`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                setComments(constructComments(data.list))
+            });
+    }
+
+    const constructComments = (comment_items) => {
+        // construct comments
+        return comment_items.map(comment => {
+            return <Comment
+                comment={{
+                    user: comment.username,
+                    content: comment.content,
+                    like: comment.like_cnt
+                }}
+                key={comment.comment_pk}
+            />
+        })
+    }
+
+
+    useEffect(() => {
+        console.log("1. 댓글 가져오기")
+        getComments();
+    }, [])
+
+
     return(
         <section className="info-comments">
             <div className="info-comments-header">댓글(15개)</div>
             <div className="info-comments-body">
-                <Comment comment={{user:"user", content:"content", like:0}} />
+                {comments}
             </div>
 
             <div className="info-comments-input">
