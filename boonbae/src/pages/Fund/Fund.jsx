d814827fe56a571b5faf0b91c4eefe945a8a6a01
bini59 <myuni0234@gmail.com>
2023-07-07 04:25:45 +0900

    feat : Item 컴포넌트 제작 완료
    - fund 변수를 이용해서 item 제작 완료

--- boonbae/src/pages/Fund/Fund.jsx
+++ boonbae/src/pages/Fund/Fund.jsx
@@ -1,6 +1,26 @@
+import { Item } from "./components/Item";
+
+import "../../styles/Fund.css"
+
 const Fund = () => {
+    const fund = {
+        pk: 0,
+        image: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",
+        title: "나무를 심어요",
+        firstCategory: "환경",
+        secondCategory: "나무",
+        description: "나무를 심어요",
+        progress: 50,
+        cur_money: 50000,
+        dday: 10,
+    }
+
     return (
-        <h1>펀딩 페이지입니다.</h1>
+        <>
+            <h1>펀딩 페이지입니다.</h1>
+            <Item fund={fund} />
+        </>
+        
     )
 }
 
