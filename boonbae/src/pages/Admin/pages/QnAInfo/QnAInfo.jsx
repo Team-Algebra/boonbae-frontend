806f1bde6af4694021fb2d3ea78f0e2275583799
chaeyeon41 <rtfgvb098@naver.com>
2023-07-08 05:48:45 +0900

    댓글 삭제 추가

--- boonbae/src/pages/Admin/pages/QnAInfo/QnAInfo.jsx
+++ boonbae/src/pages/Admin/pages/QnAInfo/QnAInfo.jsx
@@ -11,6 +11,21 @@ const QnAInfo = () => {
     const { qnaPk } = useParams();
     const [qnaInfo, setQnaInfo] = useState();
 
+    const deleteData = useCallback(() => {
+        axios({
+            method: "delete",
+            url: `${process.env.REACT_APP_PROXY}/qna/${qnaPk}/reply`
+        })
+            .then((result) => {
+                console.log(result)
+                alert("댓글이 삭제되었습니다.")
+            })
+            .catch((error) => {
+                console.log(error);
+                alert("댓글 삭제에 실패하였습니다.")
+            })
+    }, [qnaPk]);
+
     const fetchData = useCallback(() => {
         axios({
             method: "get",
@@ -32,6 +47,7 @@ const QnAInfo = () => {
         <div className="qna">
             <div className="qna-header">
                 <div onClick={() => { navigate(`/admin/qna`) }}>| 정보 Q&A</div>
+                <button className="delete-button" onClick={deleteData}>댓글 삭제</button>
             </div>
             {qnaInfo && <QnAMoreInfo qnaInfo={qnaInfo}></QnAMoreInfo>}
             <Reply qnaPk={qnaPk} isReply={qnaInfo?.[0]?.isReply}></Reply>
