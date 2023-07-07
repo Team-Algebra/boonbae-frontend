57a74bfb9f3b9f366b00ec4669eab3e817273795
OhSeChan <seb_s19@naver.com>
2023-07-07 23:51:19 +0900

    feat: 사용자 정보 불러오기 및 네비게이션 바 기본 구현
    - 마이페이지 접속 시 사용자 정보 불러오기
    - 마이페이지 좌측에 위치한 네비게이션바 기본 틀 구현

--- boonbae/src/pages/Mypage/Mypage.jsx
+++ boonbae/src/pages/Mypage/Mypage.jsx
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import '../../styles/Mypage.css'
+import './components/MypageInfo'
+import './components/MypageNav'
+import MypageInfo from './components/MypageInfo';
+import MypageNav from './components/MypageNav';
 
 const Mypage = () => {
+  const [userInfo,setUserInfo] = useState({
+    username : '',
+    id : '',
+    eco_point : '',
+    user_img : '',
+    rank : 1,
+    all_cnt : 1
+  });
+
+  const getUserInfo = async () =>{
+    try {
+      const token = localStorage.getItem('token');
+      const response = await axios.get(`${process.env.REACT_APP_PROXY}/users/info/my`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if(response.status === 200){
+        console.log(response.data);
+        setUserInfo(response.data);
+      }else{
+        console.log('사용자 정보를 불러오지 못했습니다.');
+      }
+      } catch (error) {
+        console.error('사용자 정보를 불러오지 못했습니다.',error);
+      }
+    }
+
+  useEffect(()=>{
+    getUserInfo();
+    console.log(userInfo);
+  },[])
+
   return (
     <div className='mypage'>
-      
+      <MypageNav userInfo = {userInfo}/>
+      <MypageInfo/>
     </div>
   );
 };
--- boonbae/src/pages/Mypage/components/MypageNav.jsx
+++ boonbae/src/pages/Mypage/components/MypageNav.jsx
@@ -1,10 +1,41 @@
-import React from 'react';
+import React,{useState} from 'react';
 import '../../../styles/MypageNav.css'
+import logo from '../../../assets/분리배출.PNG'
 
-const MypageNav = () => {
+const MypageNav = ({userInfo}) => {
+  const [selectedTab, setSelectedTab] = useState('');
+
+  const handleTabClick = (tabName) => {
+    setSelectedTab(tabName);
+  };
+
   return (
-    <div>
-      
+    <div className='mypage-nav'>
+      <h2>마이페이지</h2>
+      <div className='mypage-imgwrapper'>
+      <img src={userInfo.user_img ? userInfo.user_img : logo} alt='프로필 이미지' />
+      </div>
+      <span>{userInfo.username}</span>
+      <button>닉네임 수정</button>
+      <div className='mypage-nav-page'>
+        <span
+          className={selectedTab === 'info' ? 'selected' : ''}
+          onClick={() => handleTabClick('info')}
+        >
+          기본 정보
+        </span>
+        <span
+          className={selectedTab === 'funding' ? 'selected' : ''}
+          onClick={() => handleTabClick('funding')}
+        >
+          나의 펀딩
+        </span>
+      </div>
+      <div className='mypage-nav-user'>
+        <span>로그아웃</span>
+        <span>비밀번호 수정</span>
+        <span>회원탈퇴</span>
+      </div>
     </div>
   );
 };
