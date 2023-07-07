4c2ff7b1429ebf2b23233139521be8420ecc3931
bini59 <myuni0234@gmail.com>
2023-07-07 20:19:22 +0900

    feat : 검색어 설정 완료
    -  정렬기준, 카테고리, 검색어에 따라 검색할 수 있도록 검색 기준 설정 완료
    - Navbar에서 카테고리, 정렬기준 설정
    - header에서 검색어 입력

--- boonbae/src/pages/Fund/Fund.jsx
+++ boonbae/src/pages/Fund/Fund.jsx
@@ -1,4 +1,7 @@
+import React, { useState, useEffect } from "react";
+
 import { Item } from "./components/Item";
+import { Navbar } from "./components/Navbar";
 
 import "../../styles/Fund.css"
 
@@ -15,10 +18,51 @@ const Fund = () => {
         dday: 10,
     }
 
+    const fund_count = 1;
+
+    
+    
+
+    // 검색어, 카테고리, 정렬 기준 설정
+    const [fundSearch, setFundSearch] = useState({
+        keyword: "",
+        category: "",
+        sort: ""
+    })
+
+    // 검색어 설정
+    const setKeyword = (e) => {
+        let keyword = e.target.value;
+        setFundSearch({
+            ...fundSearch,
+            keyword: keyword
+        })
+    }
+
+    // test 코드, 검색어 설정이 바뀔때마다 검색어를 출력
+    // 추후 삭제 필요
+    useEffect(() => {
+        console.log(fundSearch);
+    }, [fundSearch])
+
     return (
         <>
-            <h1>펀딩 페이지입니다.</h1>
-            <Item fund={fund} />
+            <section className="fund-header">
+                <div className="fund-header-sec-1">
+                    <div className="fund-header-upload">프로젝트 올리기</div>
+                    <input className="fund-header-input" placeholder="검색어를 입력해주세요!" onKeyUp={setKeyword}/>
+                </div>
+                <Navbar fundSearch={ fundSearch } setFundSearch={ setFundSearch } />
+                <div className="fund-header-count">
+                    <span className="fund-count">{ fund_count }</span>개의 프로젝트가 있습니다.
+                </div>
+            </section>
+            <section className="fund-body">
+                <Item fund={fund} />
+            </section>
+            <section className="fund-footer">
+                <div className="fund-pagination"></div>
+            </section>  
         </>
         
     )
