9820fc0d36a3d90cee1eb1a0719ad49faf6d7edd
chaeyeon41 <rtfgvb098@naver.com>
2023-07-08 06:07:30 +0900

    답변완료 추가

--- boonbae/src/pages/Enquire/components/Paging.jsx
+++ boonbae/src/pages/Enquire/components/Paging.jsx
@@ -1,25 +1,25 @@
 import Pagination from "react-js-pagination";
 import { useNavigate } from "react-router-dom";
 
-const Paging = ({ currentPage, pageSize, qnaArray, handlePageChange })  => {
-    
+const Paging = ({ currentPage, pageSize, qnaArray, handlePageChange }) => {
+
     const navigate = useNavigate();
 
     return (
         <>
             <div className="qna-table-bottom">
                 <div className="pagination-wrapper">
-                    <Pagination 
-                        activePage = {currentPage}
-                        itemsCountPerPage = {pageSize}
-                        totalItemsCount = {qnaArray.length}
+                    <Pagination
+                        activePage={currentPage}
+                        itemsCountPerPage={pageSize}
+                        totalItemsCount={qnaArray.length}
                         pageRangeDisplayed={5}
                         prevPageText={"<"}
                         nextPageText={">"}
                         onChange={handlePageChange}
                     />
                 </div>
-                <button className="regist-button" onClick={()=>{navigate('/enquire/regist')}}>글 작성</button>
+                <button className="regist-button" onClick={() => { navigate('/enquire/regist') }}>글 작성</button>
             </div>
         </>
     )
